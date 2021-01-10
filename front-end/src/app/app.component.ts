2cea9bf7d71a505e06d3fc5457757d0824b618f0
Marios Papamichalopoulos <papamichalopoulos.m@gmail.com>
2021-01-10 18:49:55 +0200

    layout fixes

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { RoleEnum } from './_enums/role.enum';
 import { Auth } from './_models/auth.model';
+import { Role } from './_models/role.model';
 import { AuthService } from './_services/auth.service';
 import { LocalStorageService } from './_services/local-storage.service';
 
@@ -27,6 +29,7 @@ export class AppComponent {
     private authService: AuthService) {}
 
   loggedIn: Auth;
+  isAdmin: Role;
   searchString: string;
 
   ngOnInit(): void {
@@ -35,6 +38,7 @@ export class AppComponent {
 
   refreshAuthentication() {
     this.loggedIn = this.localStorageService.authFromLocalStorage;
+    this.isAdmin = this.localStorageService.rolesFromLocalStorage.find(role => RoleEnum[role.name] === RoleEnum.ROLE_ADMINISTRATOR)
   }
 
   search() {
